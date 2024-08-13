bbc69f29758c0e4717412cbc4e1711564a7c1d41
Ken Sodemann <ken@ionic.io>
2024-08-13 16:10:21 -0500

    upgrade NG

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.menukeyboardng',
   appName: 'menu-keyboard-ng',
-  webDir: 'www',
+  webDir: 'www/browser',
 };
 
 export default config;
